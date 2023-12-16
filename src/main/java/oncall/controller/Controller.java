6418b8b3ba14300ca160769ce20a0f5da1882511
wooqqq <sge0225@naver.com>
2023-12-16 14:29:09 +0900

    feat: 비상근무 배정 월과 시작 요일 입력 기능 구현

--- src/main/java/oncall/controller/Controller.java
+++ src/main/java/oncall/controller/Controller.java
@@ -4,9 +4,11 @@ import oncall.view.InputMonthView;
 
 public class Controller {
     public void run() {
+        getMonth();
     }
 
     private void getMonth() {
         InputMonthView inputMonthView = new InputMonthView();
+        inputMonthView.getMonth();
     }
 }
--- src/main/java/oncall/view/InputMonthView.java
+++ src/main/java/oncall/view/InputMonthView.java
@@ -1,5 +1,33 @@
 package oncall.view;
 
-public class InputMonthView extends InputView {
+import java.util.HashMap;
+import java.util.Map;
 
+public class InputMonthView extends InputView {
+    private static final String SEPARATOR = ",";
+    private static final String REQUEST_MONTH = "비상 근무를 배정할 월과 시작 요일을 입력하세요> ";
+    private static final String ERROR_MESSAGE = "[ERROR] 유효하지 않은 입력 값입니다. 다시 입력해 주세요.";
+
+    public Map<Integer, String> getMonth() {
+        System.out.print(REQUEST_MONTH);
+        String input = inputView();
+        try {
+            parseInputValue(input);
+        } catch (IllegalArgumentException e) {
+            System.out.println(ERROR_MESSAGE);
+            return getMonth();
+        }
+        return parseInputValue(input);
+    }
+
+    private Map<Integer, String> parseInputValue(String input) {
+        String[] inputValue = input.split(SEPARATOR);
+        Map<Integer, String> result = new HashMap<>();
+
+        // month, startDate에 대한 유효성 검사 필요
+        int month = Integer.parseInt(inputValue[0]);
+        String startDate = inputValue[1];
+        result.put(month, startDate);
+        return result;
+    }
 }
