54ac9b30e5022d298de1f853af0180194e37d2ef
wooqqq <sge0225@naver.com>
2023-12-16 14:39:14 +0900

    feat: 입력받은 월과 시작요일을 통해 enum값 찾는 기능 구현

--- src/main/java/oncall/domain/EmergencyWorkMonth.java
+++ src/main/java/oncall/domain/EmergencyWorkMonth.java
@@ -8,4 +8,22 @@ public class EmergencyWorkMonth {
         this.month = month;
         this.startDate = startDate;
     }
+
+    private Calender getCalenderByMonth(int month) {
+        for (Calender calender : Calender.values()) {
+            if (calender.getMonth() == month) {
+                return calender;
+            }
+        }
+        return null;
+    }
+
+    private Week getWeekByStartDate(String startDate) {
+        for (Week week : Week.values()) {
+            if (week.getDate().contains(startDate)) {
+                return week;
+            }
+        }
+        return null;
+    }
 }
