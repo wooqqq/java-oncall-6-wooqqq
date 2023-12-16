d8fb0eae85b53180c7d3d91df9a91e14a3df6aaf
wooqqq <sge0225@naver.com>
2023-12-16 14:32:30 +0900

    feat: 비상근무표 관리할 클래스 생성

---
+++ src/main/java/oncall/domain/EmergencyWorkMonth.java
@@ -0,0 +1,11 @@
+package oncall.domain;
+
+public class EmergencyWorkMonth {
+    private int month;
+    private String startDate;
+
+    public EmergencyWorkMonth(int month, String startDate) {
+        this.month = month;
+        this.startDate = startDate;
+    }
+}
