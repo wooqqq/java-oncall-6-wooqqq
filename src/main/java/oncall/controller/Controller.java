dac1bf4bb1b9f48836935c8deade0d60bd324b63
wooqqq <sge0225@naver.com>
2023-12-16 15:15:50 +0900

    feat: 입력받은 사원을 그룹화하는 기능 추가 후 입력 기능 도입

--- src/main/java/oncall/controller/Controller.java
+++ src/main/java/oncall/controller/Controller.java
@@ -1,13 +1,22 @@
 package oncall.controller;
 
 import oncall.domain.EmergencyWorkMonth;
+import oncall.domain.Employee;
+import oncall.domain.Employees;
+import oncall.view.InputEmployeeView;
 import oncall.view.InputMonthView;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class Controller {
+    private InputEmployeeView inputEmployeeView;
+
     public void run() {
         setEmergencyWorkMonth(getMonth());
+        Employees weekday = setEmployees(getEmployee(getWeekdayEmployee()));
+        Employees holiday = setEmployees(getEmployee(getHolidayEmployee()));
     }
 
     private EmergencyWorkMonth setEmergencyWorkMonth(Map<String, String> input) {
@@ -22,5 +31,28 @@ public class Controller {
     private Map<String, String> getMonth() {
         InputMonthView inputMonthView = new InputMonthView();
         return inputMonthView.getMonth();
+    }
+
+    private Employees setEmployees(List<Employee> employees) {
+        return new Employees(employees);
+    }
+
+    private List<Employee> getEmployee(List<String> employeeNames) {
+        inputEmployeeView = new InputEmployeeView();
+        List<Employee> employees = new ArrayList<>();
+        List<String> names = employeeNames;
+        for (String name : names) {
+            Employee employee = new Employee(name);
+            employees.add(employee);
+        }
+        return employees;
+    }
+
+    private List<String> getWeekdayEmployee() {
+        return inputEmployeeView.getWeekdayEmployee();
+    }
+
+    private List<String> getHolidayEmployee() {
+        return inputEmployeeView.getHolidayEmployee();
     }
 }
