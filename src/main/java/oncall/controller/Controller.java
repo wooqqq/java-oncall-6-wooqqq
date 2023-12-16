8e366fe73c1dfd431612417c6836a0a2bed12298
wooqqq <sge0225@naver.com>
2023-12-16 14:48:46 +0900

    feat: 입력받은 값을 비상 근무표 객체로 저장

--- src/main/java/oncall/controller/Controller.java
+++ src/main/java/oncall/controller/Controller.java
@@ -1,14 +1,26 @@
 package oncall.controller;
 
+import oncall.domain.EmergencyWorkMonth;
 import oncall.view.InputMonthView;
+
+import java.util.Map;
 
 public class Controller {
     public void run() {
-        getMonth();
+        setEmergencyWorkMonth(getMonth());
     }
 
-    private void getMonth() {
+    private EmergencyWorkMonth setEmergencyWorkMonth(Map<Integer, String> input) {
+        if (!input.isEmpty()) {
+            Integer month = input.keySet().iterator().next();
+            String startDate = input.get(month);
+            return new EmergencyWorkMonth(month, startDate);
+        }
+        return null;
+    }
+
+    private Map<Integer, String> getMonth() {
         InputMonthView inputMonthView = new InputMonthView();
-        inputMonthView.getMonth();
+        return inputMonthView.getMonth();
     }
 }
